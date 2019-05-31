0185afbe58f0243321fced70ce4b8173546bdb79
Vadim Vrublevsky <v.v.vrublevsky@gmail.com>
2019-05-31 14:02:45 +0300

    [REFACTOR] Убраны строки с временными комментариями

--- js/picture.js
+++ js/picture.js
@@ -27,57 +27,6 @@
     window.actionsWithPopup();
   };
 
-
   window.load(onDataFromServer);
-
-
-    // var pictureOpen = pictureListElement.querySelectorAll('.picture');
-    // var pictureListGallery = document.querySelector('.gallery-overlay');
-    // var pictureClose = pictureListGallery.querySelector('.gallery-overlay-close');
-
-    // // Фунции открытия и закрытия Popup'a
-    // var onPopupPressEsc = function(evt) {
-    //   if (window.util.isEscEvt(evt)) {
-    //     closePopup();
-    //   };
-    // };
-    // var openPopup = function(index) {
-    //   pictureListGallery.classList.remove('hidden');
-    //   showBigPhoto(index);
-    //   document.addEventListener('keydown', onPopupPressEsc);
-    // };
-    // var closePopup = function() {
-    //   pictureListGallery.classList.add('hidden');
-    //   document.removeEventListener('keydown', onPopupPressEsc);
-    // };
-    // var showBigPhoto = function(index) {
-    //   pictureListGallery.querySelector('.gallery-overlay-image').src = index.querySelector('img').src;
-    //   pictureListGallery.querySelector('.likes-count').textContent = index.querySelector('.picture-likes').textContent;
-    //   pictureListGallery.querySelector('.comments-count').textContent = index.querySelector('.picture-comments').textContent;
-    // };
-    // var OpenPopupClick = function(index) {
-    //   index.addEventListener("click", function (){
-    //     openPopup(index);
-    //   });
-    // };
-    // var OpenPopupKeydown = function(index) {
-    //   index.addEventListener("keydown", function (evt){
-    //     if (window.util.isEnterEvt(evt)) {
-    //       openPopup(index);
-    //     };
-    //   });
-    // };
-
-    // // События
-    // pictureOpen.forEach(OpenPopupClick);
-    // pictureOpen.forEach(OpenPopupKeydown);
-    // pictureClose.addEventListener('click', function() {
-    //   closePopup();
-    // });
-    // pictureClose.addEventListener('keydown', function(evt) {
-    //   if (window.util.isEnterEvt(evt)) {
-    //     closePopup();
-    //   };
-    // });
 
 })();
